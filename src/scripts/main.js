00bd4b16344e19843f42b9b4a7973a6fedc288d8
Cassie Boyd <cassandra.bacon@gmail.com>
2019-10-16 11:24:18 -0500

    Save and move on

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,8 +1,8 @@
 
 // 1. Use JavaScript to obtain a reference to the first section with the class of article_header and change its text with the textContent property to "Welcome to the {insert your name here} blog"
 // 2. Use JavaScript to obtain a reference to all article__header elements and change their classList property value to article__header important.
-// 3. Obtain a reference the element with a class of dashed and remove it.
-// 4. Obtain a reference the element with a class of article_footer and add the class of goldenrod it.
+// 3. Obtain a reference to the element with a class of dashed and remove it.
+// 4. Obtain a reference to the element with a class of article_footer and add the class of goldenrod it.
 
 
 // Selects the first section with the class of article_header and assigns it to 
@@ -15,4 +15,3 @@ const important = document.querySelectorAll(".article__header")
 important.forEach((tag) => {
     tag.classList.add("important")
 })
-console.log(important)
